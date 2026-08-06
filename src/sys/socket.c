5c523d02beffeb39ed8997a506b08e380dad8d5b
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-06 13:27:27 -0700

    Enforce semantic wrapper boundaries

--- src/sys/socket.c
+++ src/sys/socket.c
@@ -14,11 +14,6 @@
  * limitations under the License.
  */
 
-#include "p101_network/arpa/p101_inet.h"
-#include "p101_network/net/p101_ethernet.h"
-#include "p101_network/net/p101_if.h"
-#include "p101_network/p101_ifaddrs.h"
-#include "p101_network/p101_netdb.h"
 #include "p101_network/sys/p101_socket.h"
 #include <p101_env/wrapper.h>
 
